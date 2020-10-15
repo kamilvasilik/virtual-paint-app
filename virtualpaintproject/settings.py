525354070fe76042a93b1aceff296bd73db1be68
kamilvasilik <kamil.vasilik@gmail.com>
2020-10-15 21:39:50 +0200

    secret key

--- virtualpaintproject/settings.py
+++ virtualpaintproject/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '4w@2e1wnf+3_(4o5h)7l0z4l)yg+njmvwb_tnr^rc9wd-jv4x%'
-SECRET_KEY = config('SECERT_KEY')
+SECRET_KEY = '4w@2e1wnf+3_(4o5h)7l0z4l)yg+njmvwb_tnr^rc9wd-jv4x%'
+# SECRET_KEY = config('SECERT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
