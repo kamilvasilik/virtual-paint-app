c6ef44c0132fde38563d7f4fa37bba94e3a6be6e
kamilvasilik <kamil.vasilik@gmail.com>
2020-10-16 16:10:27 +0200

    cv2.VideoCapture(0) STATIC_URL = '/static/'

--- virtualpaintproject/settings.py
+++ virtualpaintproject/settings.py
@@ -146,8 +146,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'assets')
 
-# STATIC_URL = '/static/'
-STATIC_URL = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
